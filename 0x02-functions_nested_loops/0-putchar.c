76c69ba6811f7c4667e5c12892625484ff971a7a
Diomibus <diomibus@gmail.com>
2022-12-14 16:33:17 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by bwave
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
+
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +19,7 @@ int main(void);
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	
 	return (0);
+
 }
